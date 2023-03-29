a206768ae476057fbb46930b25ab723d2ffc0cec
saikumar <boyasaikumar542@gmail.com>
2023-03-29 12:02:52 +0530

    myfirst commit

---
+++ codecaro/Responsivecards/jsg.js
@@ -0,0 +1,14 @@
+function joined(){
+    let title=document.getElementById('title');
+    // title.innerText='yes i joiner'
+     title.innerHTML='yes i joiner'
+     title.style='color:red'
+     let btn=document.getElementById("btn")
+     btn.style.display='none'
+
+
+}
+console.log(title)
+function welcome(){
+    let name=document.getElementById(.)
+}
